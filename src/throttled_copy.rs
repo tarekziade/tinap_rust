4c0f8d25fd79593a820d57134d3158a16a3e8f7b
Tarek Ziade <tarek@ziade.org>
2019-10-18 10:38:46 +0200

    implement delay

--- src/throttled_copy.rs
+++ src/throttled_copy.rs
@@ -1,23 +1,23 @@
 // https://docs.rs/async-std/0.99.9/src/async_std/io/copy.rs.html#44-100
 
 use std::pin::Pin;
-use std::thread;
-use std::time::Duration;
 use std::ptr;
+use std::thread;
+use std::time::{Duration, Instant};
 
 use async_std::future::{ready, Future};
 use async_std::io::{self, BufRead, BufReader, Read, Write};
-use async_std::task::{sleep, Context, Poll};
+use async_std::task::{sleep, Context, Poll, Waker};
+use futures_timer::Delay;
 
 // XXX  make those options for copy()
-const DELAY: u64 = 1000;
+const DELAY: u64 = 500;
 
 #[derive(Debug)]
 enum State {
     WaitForData,
-    StartDelay,
+    Delaying,
     DelayDone,
-    BufferEmpty,
 }
 
 pub async fn copy<R, W>(reader: &mut R, writer: &mut W) -> io::Result<u64>
@@ -30,26 +30,33 @@ where
         writer: &'a mut W,
         amt: u64,
         state: State,
-        buffer: Vec<u8>
+        buffer: Vec<u8>,
+        delay: Option<Delay>,
     }
 
     impl<R, W: Unpin + ?Sized> CopyFuture<'_, R, W> {
-        fn project(self: Pin<&mut Self>) -> (Pin<&mut R>, Pin<&mut W>, &mut u64, &mut Vec<u8>, &mut State) {
+        fn project(
+            self: Pin<&mut Self>,
+        ) -> (
+            Pin<&mut R>,
+            Pin<&mut W>,
+            &mut u64,
+            &mut Vec<u8>,
+            &mut State,
+            &mut Option<Delay>,
+        ) {
             unsafe {
-                println!("{:p}", self);
                 let this = self.get_unchecked_mut();
-                println!("{:p}", this);
-
                 (
                     Pin::new_unchecked(&mut this.reader),
                     Pin::new(&mut *this.writer),
                     &mut this.amt,
                     &mut this.buffer,
-                    &mut this.state
+                    &mut this.state,
+                    &mut this.delay,
                 )
             }
         }
-
     }
 
     impl<R, W> Future for CopyFuture<'_, R, W>
@@ -59,70 +66,66 @@ where
     {
         type Output = io::Result<u64>;
 
-
         fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-            let (mut reader, mut writer, amt, buffer, state) = self.project();
-                println!("{:?}", state);
-                loop {
-                match state {
-                    State::WaitForData => {
-
-                        *state = State::StartDelay;
-                        let b = futures_core::ready!(reader.as_mut().poll_fill_buf(cx))?;
-                        buffer.extend(b);
-                        println!("buffer 1");
-
-                        if buffer.is_empty() {
-                            println!("buffer EOF");
-                            *state = State::BufferEmpty;
-                            return Poll::Pending;
-                        }
-
-                        println!("buffer 2");
-
-                        *state = State::StartDelay;
-                        println!("buffer 3");
-
-                        return Poll::Pending;
-                    }
-
-                    State::StartDelay => {
-                        println!("StartDelay");
-                        // here we want to check how long we've waited and then change the state
-                        *state = State::DelayDone;
-                        return Poll::Pending;
-                    }
-
-                    State::DelayDone => {
-                        println!("DelayDone");
-                        let i = futures_core::ready!(writer.as_mut().poll_write(cx, &buffer))?;
-                        if i == 0 {
-                            return Poll::Ready(Err(io::ErrorKind::WriteZero.into()));
-                        }
-                        *amt += i as u64;
-                        reader.as_mut().consume(i);
-                        *state = State::WaitForData;
-                        return Poll::Pending;
-                    }
-
-                    State::BufferEmpty => {
-                        println!("BufferEmpty");
+            let (mut reader, mut writer, amt, buffer, state, delay) = self.project();
+            match (state) {
+                State::WaitForData => {
+                    let data = futures_core::ready!(reader.as_mut().poll_fill_buf(cx))?;
+                    if data.is_empty() {
                         futures_core::ready!(writer.as_mut().poll_flush(cx))?;
-                        buffer.clear();
                         *amt = 0;
                         return Poll::Ready(Ok(*amt));
                     }
+
+                    buffer.extend(data);
+                    cx.waker().wake_by_ref();
+                    *state = State::Delaying;
+                    return Poll::Pending;
+                } // end WaitForData
+
+                State::Delaying => {
+                    let mut sdelay =
+                        delay.get_or_insert_with(|| Delay::new(Duration::from_millis(DELAY)));
+                    let pdelay = Pin::new(&mut sdelay);
+                    let res = match pdelay.poll(cx) {
+                        Poll::Ready(()) => {
+                            cx.waker().wake_by_ref();
+                            *state = State::DelayDone;
+                            return Poll::Pending;
+                        }
+                        Poll::Pending => {
+                            cx.waker().wake_by_ref();
+                            *state = State::Delaying;
+                            return Poll::Pending;
+                        }
+                    };
+                } // end State::Delaying
+
+                State::DelayDone => {
+                    // writing
+                    let i = futures_core::ready!(writer.as_mut().poll_write(cx, buffer))?;
+                    if i == 0 {
+                        return Poll::Ready(Err(io::ErrorKind::WriteZero.into()));
+                    }
+                    *amt += i as u64;
+                    reader.as_mut().consume(i);
+                    delay.take();
+                    buffer.clear();
+                    *state = State::WaitForData;
+                    cx.waker().wake_by_ref();
+                    return Poll::Pending;
                 }
-                }
-        }
-    }
+            } // end match
+        } // end poll
+    } // end Future
 
     let future = CopyFuture {
         reader: BufReader::new(reader),
         writer,
         amt: 0,
         state: State::WaitForData,
-        buffer: Vec::with_capacity(1024)
+        buffer: Vec::with_capacity(1024),
+        delay: None,
     };
     future.await
 }
